fea940d888f2d69d6d01809fc085e24f9cfb8a64
Diwank Singh Tomer <diwank.singh@gmail.com>
2019-12-26 21:48:15 +0530

    Fix word boundary issue for keywords
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- graql-mode.el
+++ graql-mode.el
@@ -21,29 +21,40 @@
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
 (setq graql-font-lock-keywords
-      (let* ()
-            ;; define several category of keywords
-            (x-keywords '("define" "undefine" "match" "get" "insert" "delete" "compute"))
-            (x-types '("long" "double" "string" "boolean" "date" "thing" "entity" "attribute" "relation" "role" "rule"))
-            (x-constants '("true" "false" "abstract" "id" "asc" "desc" "type"))
-            (x-events '("offset" "limit" "group" "sort" "datatype" "key" "has" "plays" "relates"))
-            (x-functions '("centrality" "using" "or" "not" "as" "via" "like" "when" "then" "regex"))
+   (let* (
+     ;; define several category of keywords
+     (x-keywords
+      '("define" "undefine" "match" "get" "insert" "delete" "compute"))
+     (x-types
+      '("long" "double" "string" "val" "boolean" "date" "thing" "entity"
+        "attribute" "relation" "role" "rule"))
+     (x-constants
+      '("true" "false" "abstract" "id" "asc" "desc" "type"
+        "aggregate"))
+     (x-events
+      '("offset" "limit" "group" "order by" "datatype" "key" "of" "in" "when"
+        "then" "if" "has" "plays" "relates" "involves"
+        "sub!" "isa!" "sub" "isa"))
+     (x-functions
+      '("centrality" "is-abstract" "using" "or" "not" "as" "via" "like"
+        "contains" "regex" "max" "min" "std" "mean" "median" "sum" "count"
+        "degree" "path" "cluster"))
 
-            ;; generate regex string for each category of keywords
-            (x-keywords-regexp (regexp-opt x-keywords 'words))
-            (x-types-regexp (regexp-opt x-types 'words))
-            (x-constants-regexp (regexp-opt x-constants 'words))
-            (x-events-regexp (regexp-opt x-events 'words))
-            (x-functions-regexp (regexp-opt x-functions 'words))
+         ;; generate regex string for each category of keywords
+     (x-keywords-regexp (regexp-opt x-keywords 'symbols))
+     (x-types-regexp (regexp-opt x-types 'symbols))
+     (x-constants-regexp (regexp-opt x-constants 'symbols))
+     (x-events-regexp (regexp-opt x-events 'symbols))
+     (x-functions-regexp (regexp-opt x-functions 'symbols)))
 
-        `(
-          (,x-types-regexp . font-lock-type-face)
-          (,x-constants-regexp . font-lock-constant-face)
-          (,x-events-regexp . font-lock-builtin-face)
-          (,x-functions-regexp . font-lock-function-name-face)
-          (,x-keywords-regexp . font-lock-keyword-face))))
-          ;; note: order above matters, because once colored, that part won't change.
-          ;; in general, put longer words first
+     `(
+       (,x-types-regexp . font-lock-type-face)
+       (,x-constants-regexp . font-lock-constant-face)
+       (,x-events-regexp . font-lock-builtin-face)
+       (,x-functions-regexp . font-lock-function-name-face)
+       (,x-keywords-regexp . font-lock-keyword-face))))
+       ;; note: order above matters, because once colored, that part won't change.
+       ;; in general, put longer words first
 
 
 ;;;###autoload
@@ -52,6 +63,8 @@
 
   ;; code for syntax highlighting
   (setq font-lock-defaults '((graql-font-lock-keywords))))
+
+(add-to-list 'auto-mode-alist '("\\.graql\\'" . graql-mode))
 
 ;; add the mode to the `features' list
 (provide 'graql-mode)
